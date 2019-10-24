7c55476a4727a9f76e121d2a045f1deb6678cb2b
Adriana Sawyer <asawyer@ibm.com>
2019-10-24 10:39:56 -0400

    changed err msg and added comment

--- helloworld-test-sample/src/test/runTest.ts
+++ helloworld-test-sample/src/test/runTest.ts
@@ -13,9 +13,10 @@ async function main() {
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 		// Download VS Code, unzip it and run the integration test
+		//Params added to run test must align with tests present in `extension.test.ts`
 		await runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
-		console.error('Failed to run tests');
+		console.error('Unable to download VS Code and Dependency Package.');
 		process.exit(1);
 	}
 }
